a12b3ba32de305408752911a9994e847919e5194
prologue032053-byte <prologue032053@gmail.com>
2026-06-01 10:16:31 +0800

    Create module.js

---
+++ DOVis/frontend/config/module.js
@@ -0,0 +1,10 @@
+import HypoxiaModule from "../module/HypoxiaModule";
+
+export const moduleConfig = [
+  {
+    id: "hypoxia",
+    title: "Hypoxic Boundary",
+    component: HypoxiaModule,
+    props: {}
+  }
+];
