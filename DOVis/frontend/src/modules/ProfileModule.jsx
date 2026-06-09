4ccefc45e949e5d47418695c50d1a8f3499f261c
Hungerbar <3485509660@qq.com>
2026-06-10 00:10:42 +0800

    change icons

--- DOVis/frontend/src/modules/ProfileModule.jsx
+++ DOVis/frontend/src/modules/ProfileModule.jsx
@@ -23,10 +23,11 @@ export default function ProfileModule({ hidden }) {
     clearSectionPoints,
 
     reset,
+    cleanup,
   } = useProfile();
 
   const handleClose = () => {
-    reset();
+    cleanup();
     hidden();
   };
 
